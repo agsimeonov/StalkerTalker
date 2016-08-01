fd4a98ffbe8720ab86271e76a8e7aa6b1b3a0f7b
Alexander Simeonov <agsimeon@buffalo.edu>
2016-07-31 23:55:10 -0400

    Guide can now generate proper xmltv from the database.

--- guide.py
+++ guide.py
@@ -1,11 +1,17 @@
+from datetime import datetime
+from xml.etree.ElementTree import Element, SubElement, tostring
+
+from pytz import utc
+
 from client import StalkerClient
 from common import get_data
 
 
 class Guide(StalkerClient):
-  def __init__(self, television):
+  def __init__(self, television, database):
     StalkerClient.__init__(self, television.stalker_session)
     self.television = television
+    self.database = database
 
   def get_week(self):
     payload = {
@@ -26,3 +32,42 @@ class Guide(StalkerClient):
       'date': day
     }
     return self.paginate(payload)
+
+  def generate_xmltv(self):
+    parent = Element('tv')
+    self.generate_xmltv_channels(parent)
+    self.generate_xmltv_programmes(parent)
+    return tostring(parent, encoding='utf8', method='xml')
+
+  def generate_xmltv_channels(self, parent):
+    for key in self.television.channels_by_id.keys():
+      channel_attributes = { 'id': key }
+      channel = SubElement(parent, 'channel', channel_attributes)
+      display_name = SubElement(channel, 'display-name')
+      display_name.text = self.television.channels_by_id[key]['name']
+
+  def generate_xmltv_programmes(self, parent):
+    programmes = self.database.get_epg(10)
+    for element in programmes:
+      start = datetime.fromtimestamp(element['start_timestamp'], tz=utc)
+      stop = datetime.fromtimestamp(element['stop_timestamp'], tz=utc)
+      start = self.format_time(start)
+      stop = self.format_time(stop)
+      attributes = {
+        'start': start,
+        'stop': stop,
+        'channel': str(element['ch_id'])
+      }
+      programme = SubElement(parent, 'programme', attributes)
+      title = SubElement(programme, 'title')
+      title.text = element['name'].decode('utf8')
+      if element['descr'].decode('utf8'):
+        description = SubElement(programme, 'description')
+        description.text = element['descr'].decode('utf8')
+
+  def format_time(self, programme_datetime):
+    return programme_datetime.strftime('%Y%m%d%H%M%S %z')
+
+  def write_guide(self, filepath):
+    with open(filepath, 'w') as output:
+      output.write(self.generate_xmltv())
